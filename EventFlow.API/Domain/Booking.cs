8c65b4a31a9a99d65ba6b81ba66cffe6d70904d0
Bonolo Selowa <bonololloyd003@gmail.com>
2026-02-19 18:31:55 +0200

    updated

--- EventFlow.API/Domain/Booking.cs
+++ EventFlow.API/Domain/Booking.cs
@@ -5,8 +5,8 @@
         public Guid Id { get; init; }
         public Guid EventId { get; init; }
         public Event Event { get; init; } = null!;
-        public Guid UserId { get; init; }
-        public User User { get; init; } = null!;
+        //public Guid UserId { get; init; }
+        //public User User { get; init; } = null!; not needed for now users dont have to login
         public int Tickets { get; init; } = 1;// quantity reserved for now
         public DateTime CreatedAt { get; init; } = DateTime.UtcNow;
         public bool Cancelled { get; set; } = false;
